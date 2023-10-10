cedcf8eb7f35d80279bfea684f0c34c8ee6f8dd0
Alyson <aly.vinicius@icloud.com>
2023-10-10 19:14:19 -0300

    Funcionalidade tela de login

--- sessao.php
+++ sessao.php
@@ -1,41 +1,32 @@
 <?php
     session_start();
-	$login = $_POST['nm_usuario'];
+	$login = $_POST['ds_usuario'];
 	$senha = $_POST['ds_senha'];
 	
-	// A include carrega o arquivo conexao.php
+	
 	include 'conecta.php';
 	
-	$result = mysqli_query($con,"select ds_usuario,ds_senha from Vendedor where ds_usuario = '$login' and ds_senha = '$senha'");
+	$result = mysqli_query($con,"select ds_usuario,ds_senha from vendedor where ds_usuario = '$login' and ds_senha = '$senha'");
     	
-	/* Logo abaixo tem um bloco com if e else, verificando se a variável $result foi bem sucedida, 
-	ou seja se ela estiver encontrado algum registro idêntico o seu valor será igual a 1, 
-	se não, se não tiver registros seu valor será 0. */
-	/*
-	mysqli_num_rows - É a função que retorna o nr de linhas de um result set	
-	*/
+	
 
 	if(mysqli_num_rows ($result) > 0 ) { 
 
 		$_SESSION['txtnome']   = $login; 
 		$_SESSION['txtsenha']  = $senha; 
-		/*
-		isset - É a função que retorna verdadeiro se a variável existe
-		*/
+		
 		if(isset($_SESSION['txtnome'])){							
-				header('location:index.html'); 
+				header('location:menu.html'); 
 			}
 		else
 			{
-			   header('location:login.html'); 			}
+			   header('location:index.html'); 			}
 	} 
 	else{ 	    
-		/*
-	     unset - É a função que destroi a variável de sessão		
-		*/
+		
 		unset ($_SESSION['txtnome']); 
 		unset ($_SESSION['txtsenha']);
-		header('location:login.html');
+		header('location:index.html');
 	}
 
 ?>	
