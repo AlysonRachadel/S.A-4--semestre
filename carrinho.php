15a2e2e0342aa3389d9dd662cd36d41769b4426d
Alyson <aly.vinicius@icloud.com>
2023-11-13 22:18:57 -0300

    Implementação da página carrinho

--- carrinho.php
+++ carrinho.php
@@ -15,161 +15,57 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <script src="script.js"></script>
-
-    <script>
-        function apagar(id) {
-            if (window.confirm("Confirma a exclusão ? ")) {
-                window.location = "delprod.php?id=" + id;
-            }
-        }
-    </script>
 </head>
-<?php
-require 'conecta.php';
-$sql = 'select * from cliente';
-
-$query = mysqli_query($con, $sql);
-?>
 
 <body>
 
     <nav class="navbar navbar-expand-lg justify-content-between">
-        <a class="navbar-brand" href="indexx.html"> <img class="logo" src="img/logo nome.png"></a>
+        <a class="navbar-brand" href="index.html"> <img class="logo" src="img/logo nome.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <?php
+        $itemCarrinho = $_GET['item'] ?? null;
+        $subitensCarrinho = $_GET['subitens'] ?? null;
 
-        <form class="form-inline">
-            <a href="cadprod.html">
-                <button type="button" id="botao" class="btn">Registrar</button>
-            </a>
-            <a href="consultacliente.php">
-                <button type="button" id="botao" class="btn">Consultar</button>
-            </a>
-            <a href="index.html">
-                <button type="button" id="botao" class="btn">Logar</button>
-            </a>
-        </form>
+        echo 'Item do carrinho: ' . htmlspecialchars(urldecode($itemCarrinho)) . '<br>';
+
+        if ($subitensCarrinho) {
+            $subitens = explode(',', urldecode($subitensCarrinho));
+            echo 'Subitens do carrinho:<br>';
+            foreach ($subitens as $subitem) {
+                echo '- ' . htmlspecialchars($subitem) . '<br>';
+            }
+        }
+        ?>
+
+        <div class="col-md-6 footer-column">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <span class="footer-heading"><img src="img/logo flux.png" class="logoroda">
+                        <br>
+                        Flux System</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">h.xavier1232012@gmail.com</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">alyson.vinicius@icloud.com</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+        </div>
 
-    <div align="center" class="container ">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="table-responsive ">
-                    <table class="table table-bordered table-striped table-hover table-condensed ">
-                        <thead>
-                            <tr class="">
-                                <th>ID</th>
-                                <th>Nome</th>
-                                <th>Email</th>
-                                <th>Telefone</th>
+        <div class="row text-center">
+            <div class="col-md-2 box">
+                <span class="copyright quick-links">Copyright &copy; Flux System>
 
-                            </tr>
-                        </thead>
-                        <?php
-                        $contlin = 2;
-                        /*
-								 mysqli_fetch_array - Retorna o campo, a posição do array
-								*/
-                        while ($reg_cadastro = mysqli_fetch_array($query)) {
-                            $idCliente = $reg_cadastro["idCliente"];
-                            $nm_cliente = $reg_cadastro["nm_cliente"];
-                            $ds_email = $reg_cadastro["ds_email"];
-                            $ds_telefone = $reg_cadastro["ds_telefone"] . "<br>";
-
-                        ?>
-                            <tr class="info">
-                                <?php
-                                if ($contlin % 2 == 0) {
-                                ?>
-                            <tr class="trpar">
-                            <?php
-                                }
-                            ?>
-                            <td>
-                                <?php echo $idCliente ?>
-                            </td>
-                            <td>
-                                <?php echo $nm_cliente ?>
-                            </td>
-                            <td>
-                                <?php echo $ds_email ?>
-                            </td>
-                            <td>
-                                <?php echo $ds_telefone ?>
-                            </td>
-
-
-                            <td><a href="#" onclick="apagar('<?php echo $reg_cadastro["
-                                    idCliente"] ?>');"><button>Excluir</button></a></td>
-                            </tr>
-
-                        <?php
-                            $contlin = $contlin + 1;
-                        }
-                        ?>
-                    </table>
-                </div>
+                </span>
             </div>
         </div>
-    </div>
-
-    <div class='col-lg-offset-10'>
-        <a href="indexx.html"><button class="btn" id="bacana"> Voltar</button></a>
-    </div>
-
-
-    <footer>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-6 footer-column border-right">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <span class="footer-heading"><img src="img/logo lateral.png" class="logoroda">
-                                <br>
-                                Da Hora Pizzaria</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="#"> (47) 9 9925-3412</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link bi bi-facebook" href="#"> Da Hora pizzaria</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Dahorapizza@gmail.com</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-md-6 footer-column">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <span class="footer-heading"><img src="img/logo flux.png" class="logoroda">
-                                <br>
-                                Flux System</span>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">h.xavier1232012@gmail.com</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">alyson.vinicius@icloud.com</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div class="row text-center">
-                <div class="col-md-2 box">
-                    <span class="copyright quick-links">Copyright &copy; Flux System
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script>
-                    </span>
-                </div>
-            </div>
         </div>
-    </footer>
+        </footer>
+        <script src="script.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,66 +1,72 @@
-const registerBtn = document.getElementById('register');
-const container = document.getElementById('container');
+function login() {
 
-const loginBtn = document.getElementById('login');
+  const registerBtn = document.getElementById('register');
+  const container = document.getElementById('container');
+
+  const loginBtn = document.getElementById('login');
 
 
-registerBtn.addEventListener('click', () => {
-  container.classList.add("active");
-});
+  registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+  });
 
-loginBtn.addEventListener('click', () => {
-  container.classList.remove("active");
-});
+  loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+  });
 
-const myModal = document.getElementById('teste')
-const myInput = document.getElementById('testee')
+  const myModal = document.getElementById('teste')
+  const myInput = document.getElementById('testee')
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
+  myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+  })
+}
 
 
 function adicionarcarrinho() {
-console.log('funciona')
+  var combofamilia = document.getElementById('combofamilia');
+  var erromsg = document.getElementById('erromsg');
+  erromsg.textContent = '';
 
-  //Obtém a referência para o Combo 
+  // Coleta os subitens selecionados
+  var subitens = combofamilia.querySelectorAll('input[type="checkbox"]:checked');
+  var subitensValores = [];
 
-  var combofamilia = document.getElementById('combofamilia')
- 
-// Obtém a referência para a label de mensagem de erro
+  subitens.forEach(function (subitem) {
+    subitensValores.push(subitem.value);
+  });
 
+  if (subitensValores.length > 0) {
+    // Adiciona o item principal e os subitens à URL
+    var valorcombofamilia = "Item principal - R$ 85.00";
+    var url = 'carrinho.php?item=' + encodeURIComponent(valorcombofamilia);
+    url += '&subitens=' + encodeURIComponent(subitensValores.join(','));
 
-  //verifica se a checkbox está marcada 
+    console.log('Redirecionando para:', url);
 
-  if (checkbox.checked) {
-
-    //Obtém o valor da checkbox 
-
-    var valorCombo = checkbox.value;
-
-    //Adiciona o valor ao carrinho 
-
-    adicionaritem(valorCombo);
-
+    // Redireciona para a página do carrinho
+    window.location.href = url;
   } else {
-    erromsg.textContent = "Selecione pelo menos um produto para adicionar ao carrinho";
-
+    console.log('Nenhum subitem selecionado. Exibindo mensagem de erro.');
+    erromsg.textContent = "Selecione pelo menos um sabor";
   }
-
 }
 
-function adicionaritem() {
+var btnCarrinho = document.getElementById('btn-carrinho');
+btnCarrinho.addEventListener('click', adicionarcarrinho);
+
+
+/*
+function adicionaritem(item) {
+  console.log('Função adicionaritem executada com item:', item);
+
   // Obtém a referência para o carrinho 
-
   var carrinho = document.getElementById('carrinho');
-
-  //Cria um novo item da lista para o carrinho 
-
+  console.log('Elemento carrinho:', carrinho);
+  // Cria um novo item da lista para o carrinho 
   var li = document.createElement('li');
   li.textContent = item;
 
-  //Adiciona o item ao carrinho 
-
+  // Adiciona o item ao carrinho 
   carrinho.appendChild(li);
-}
+}*/ 
