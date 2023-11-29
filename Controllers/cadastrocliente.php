ea34a22c2b0dac6bf6d0039751480dc69578a549
Alyson <aly.vinicius@icloud.com>
2023-11-28 22:14:40 -0300

    Finalização

--- Controllers/cadastrocliente.php
+++ Controllers/cadastrocliente.php
@@ -1,28 +1,27 @@
-	<?php
-	/*
-	include - Faz a inclusão do arquivo, permitindo executar a lógica do programa que se encontra neste arquivo
-	
-	*/
-	include 'conecta.php';
-	/*
-	$_POST - É uma variável global que captura o conteúdo que está no formulário por meio da propriedade name		
-	*/
+<?php
+include 'conecta.php';
 
-	//Passando os dados que estão no formulário para as variáveis abaixo
-	$ds_nomec = $_POST['ds_nomec'];
-	$ds_telefone = $_POST['ds_telefone'];
-	$ds_email = $_POST['ds_email'];
-	$ds_senhac = $_POST['ds_senhac'];
-	
+// Verificar se todos os campos do formulário foram preenchidos
+if (empty($_POST['ds_nomec']) || empty($_POST['ds_telefone']) || empty($_POST['ds_email']) || empty($_POST['ds_senhac'])) {
+	echo "<script>alert('Por favor, preencha todos os campos do formulário.');window.location='../views/login.html'</script>";
+	exit(); // Encerrar o script se algum campo estiver vazio
+}
 
-	/*
-	mysqli_query - É a função do php para execução de script de inserção da tabela
-	*/
+// Passando os dados que estão no formulário para as variáveis abaixo
+$ds_nomec = $_POST['ds_nomec'];
+$ds_telefone = $_POST['ds_telefone'];
+$ds_email = $_POST['ds_email'];
+$ds_senhac = $_POST['ds_senhac'];
 
-	mysqli_query($con, "insert into cliente(nm_cliente, ds_telefone,ds_email, ds_password)
-		 values('$ds_nomec','$ds_telefone','$ds_email','$ds_senhac')");
+// Verificar se o e-mail já está cadastrado
+$result = mysqli_query($con, "SELECT * FROM cliente WHERE ds_email = '$ds_email'");
+if (mysqli_num_rows($result) > 0) {
+	echo "<script>alert('Este e-mail já está cadastrado. Por favor, escolha outro.');window.location='../views/login.html'</script>";
+	exit(); // Encerrar o script se o e-mail já estiver cadastrado
+}
 
-	echo "<script>alert('Inclusão realizada com sucesso');</script>";
-	echo "<script>window.location='../views/login.html'</script>";
+// Inserir os dados na tabela
+mysqli_query($con, "INSERT INTO cliente(nm_cliente, ds_telefone, ds_email, ds_password) VALUES ('$ds_nomec', '$ds_telefone', '$ds_email', '$ds_senhac')");
 
-	?>
+echo "<script>alert('Inclusão realizada com sucesso');</script>";
+echo "<script>window.location='../views/login.html'</script>";
--- Controllers/sessao.php
+++ Controllers/sessao.php
@@ -1,32 +1,25 @@
 <?php
-    session_start();
-	$login = $_POST['ds_email'];
-	$senha = $_POST['ds_senhac'];
-	
-	
-	include 'conecta.php';
-	
-	$result = mysqli_query($con,"select ds_email,ds_password from cliente where ds_email = '$login' and ds_password = '$senha'");
-    	
-	
+session_start();
+$login = $_POST['ds_email'];
+$senha = $_POST['ds_senhac'];
 
-	if(mysqli_num_rows ($result) > 0 ) { 
+include 'conecta.php';
 
-		$_SESSION['txtemail']   = $login; 
-		$_SESSION['txtsenha']  = $senha; 
-		
-		if(isset($_SESSION['txtemail'])){							
-				header('location:../views/index.html'); 
-			}
-		else
-			{
-			   header('location:../views/login.html'); 			}
-	} 
-	else{ 	    
-		
-		unset ($_SESSION['txtemail']); 
-		unset ($_SESSION['txtsenha']);
-		header('location:../views/login.html');
-	}
+// Usando instruções preparadas para evitar injeção de SQL
+$stmt = $con->prepare("SELECT ds_email, ds_password FROM cliente WHERE ds_email = ? AND ds_password = ?");
+$stmt->bind_param("ss", $login, $senha);
+$stmt->execute();
+$stmt->store_result();
 
-?>	
+if ($stmt->num_rows > 0) {
+	$_SESSION['txtemail'] = $login;
+	$_SESSION['txtsenha'] = $senha;
+	header('location:../views/index.html');
+} else {
+	unset($_SESSION['txtemail']);
+	unset($_SESSION['txtsenha']);
+	echo '<script>alert("Email ou senha incorretos"); window.location="../views/login.html";</script>';
+}
+
+$stmt->close();
+$con->close();
